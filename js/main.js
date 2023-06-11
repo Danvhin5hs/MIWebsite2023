f10c31c3017d8ece7c7786cde9058fb04215a488
Danvhin5hs <Vramgallerdi1986puterxd@gmail.com>
2023-06-10 22:57:05 -0500

    añadido javascript reglas dom 1

--- js/main.js
+++ js/main.js
@@ -1,7 +1,25 @@
-$(document).ready(function() {
-  const container = $('#container');
-  
-  const newDiv = $('<div>').text('Contenido del nuevo div').addClass('custom-div');
-  
-  container.append(newDiv);
-});
+
+  function mostrar() {
+
+    var removeButton = document.getElementById("dota");
+    removeButton.remove();
+
+
+    const newDiv = document.createElement("div");
+    newDiv.id = "myNewDiv";
+    
+    document.getElementById('myNewDiv').innerHTML =+ '<p>No hay Dota ps CTM</p>';
+
+
+
+    var container = document.getElementById("footer");
+    container.appendChild(newDiv);
+
+
+
+   /*  var element = document.getElementById("dota");
+    
+    element.innerHTML = "<p>dota</p>"; */
+  }
+
+
